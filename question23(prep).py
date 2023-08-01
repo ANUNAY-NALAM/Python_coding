1254d432fdc0bb6166bec2586d44b5835c7edc73
ANUNAY-NALAM <79961176+ANUNAY-NALAM@users.noreply.github.com>
2023-08-01 21:18:06 +0000

    ques23

---
+++ question23(prep).py
@@ -0,0 +1,30 @@
+
+def method2(arr,y):
+  n=len(arr)
+  s=0
+  for i in range(n):
+    diff=arr[i]-y*(i+1)
+    s+=max(diff,0)
+  return s
+
+def method1(arr,k):
+  left,right=0,max(arr)
+  res_y,res_s=-1,-1
+  while left<=right:
+    mid= (left+right)//2
+    s=method2(arr,mid)
+    if s>=k:
+      res_y=mid
+      res_s=s
+      left=mid+1
+    else:
+      right=mid-1
+  return res_y,res_s
+
+if __name__ == '__main__':
+  t=int(input())
+  for _ in range(t):
+    n,k=map(int,input().split())
+    arr=list(map(int,input().split()))
+    y,s=method1(arr,k)
+    print(y,s)
